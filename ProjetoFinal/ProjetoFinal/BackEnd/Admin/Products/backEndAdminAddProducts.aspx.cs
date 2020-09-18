cef1bc706c0f8119d9b4d46d9c89dc5cd99e1c8f
Antonio <antsal17@gmail.com>
2020-09-18 01:05:54 +0100

    Revert "lol"
    This reverts commit 2bf730e706718d2d6c8f0271cea1c880350d8d33, reversing
    changes made to b56c1c224125828a8661b42a36264b25861208e8.

--- ProjetoFinal/ProjetoFinal/BackEnd/Admin/Products/backEndAdminAddProducts.aspx.cs
+++ ProjetoFinal/ProjetoFinal/BackEnd/Admin/Products/backEndAdminAddProducts.aspx.cs
@@ -16,6 +16,38 @@ namespace ProjetoFinal.BackEnd.Admin.Products
 
         }
 
-        
+        protected void btn_AddPdc_Click(object sender, EventArgs e)
+        {
+            string retorno = "";
+            // nome descricao status qtd preco id_categoria fotoMain foto1 foto2 foto3 idUser idMorada
+
+                retorno = DBConnections.insereProduto(validationCustom18.Text.ToString(), validationCustom12.Value.ToString(), true,  Convert.ToInt32(validationCustom24.Text), Convert.ToDouble(validationCustom25.Text), Convert.ToInt32(validationCustom22.SelectedValue.ToString()), image(validatedCustomFile, lblMain), image(validatedCustomFile1, lblImage1), image(validatedCustomFile2, lblImage2), image(validatedCustomFile3, lblImage3), Convert.ToInt32(DropDownList2.SelectedValue.ToString()), Convert.ToInt32(DropDownList1.SelectedValue.ToString()));
+
+            if (retorno == "0")
+            {
+                //utilizador já existe
+                lbl_insertMsg.Visible = true;
+                lbl_insertMsg.Text = "Produto já existe!";
+            }
+            else if (retorno == "1")
+            {
+                lbl_insertMsg.Visible = true;
+                lbl_insertMsg.Text = "Produto inserido com sucesso!";
+                //inserido
+            }
+        }
+
+        string image(HtmlInputFile file, Label label)
+        {
+            if (file.PostedFile != null && file.PostedFile.ContentLength > 4000)
+            {
+                string nomeFicheiro = file.PostedFile.FileName.ToString();
+                file.PostedFile.SaveAs(Server.MapPath("..//..//..//Imagens//produto//" + validationCustom18.Text.ToString() + DropDownList2.SelectedValue.ToString() + DropDownList1.SelectedValue.ToString() + nomeFicheiro));
+                string nomeFinal = validationCustom18.Text.ToString() + DropDownList2.SelectedValue.ToString() + DropDownList1.SelectedValue.ToString() + nomeFicheiro;
+                label.Text = nomeFinal;
+                return nomeFinal;
+            }
+            return "lol";
+       }
     }
 }
